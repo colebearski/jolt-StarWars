abe497bce4ac960551577e1540daf7dc370ab505
Cole Pedersen <colepedersen@Coles-MacBook-Pro.local>
2018-08-16 09:30:03 -0600

    1.1 all but pic

--- src/Card.js
+++ src/Card.js
@@ -1,25 +1,32 @@
-import React, { Component } from 'react';
-import './Card.css';
+import React, { Component } from "react";
+import "./Card.css";
+
+// 1. Modify the *Card* component to take props for the
+// person's name, image, birthday, and home planet.
 
 class Card extends Component {
   render() {
-    return (
-      <div className='card'>
-        <div className='card-content'>
-          	<div className='card-name'>Darth Vader</div>
-          	<img src='http://localhost:3008/darth_vader.jpg' alt='profile'/>
-            <p>
-                <span>Birthday:</span>
-                <span>41.9BBY</span>
-            </p>
-            <p>
-                {/* Note that in order to get the homeworld's name, you have to get the planet name from a different endpoint than the people */}
-                <span>Homeworld:</span>
-                <span>Tatooine</span>
-            </p>
-        </div>
-    </div>
+    const name = this.props.name;
+    // const photo = this.photo.props;
+    const birthday = this.props.birthday;
+    const home = this.props.home;
 
+    return (
+      <div className="card">
+        <div className="card-content">
+          <div className="card-name">{name}</div>
+          <img src="http://localhost:3008/darth_vader.jpg" alt="profile" />
+          <p>
+            <span>Birthday:</span>
+            <span>{birthday}</span>
+          </p>
+          <p>
+            {/* Note that in order to get the homeworld's name, you have to get the planet name from a different endpoint than the people */}
+            <span>Homeworld:</span>
+            <span>{home}</span>
+          </p>
+        </div>
+      </div>
     );
   }
 }
