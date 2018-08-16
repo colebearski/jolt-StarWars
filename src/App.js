11432fac70c06ee031ae53baa5fd7e0587f7ea44
Cole Pedersen <colepedersen@Coles-MacBook-Pro.local>
2018-08-16 09:52:35 -0600

    1.2 rendered complete cards

--- src/App.js
+++ src/App.js
@@ -5,30 +5,24 @@ import SearchBar from "./SearchBar.js";
 import star from "./images/star.svg";
 import wars from "./images/wars.svg";
 import axios from "axios";
-import { link } from "fs";
 
 // 2. GET the list of Star Wars people from `http://localhost:3008/people`
 // and render a *Card* component for each of the people, using people data
 // from the API as props.
 // https://alligator.io/react/axios-react/
 
-
-
 class App extends Component {
-
   state = {
     persons: []
-  }
-  
-  componentDidMount () {
+  };
+
+  componentDidMount() {
     axios.get(`http://localhost:3008/people`).then(res => {
       const persons = res.data;
-      this.setState({ persons })
-      console.log(persons)
-    })
+      this.setState({ persons });
+      console.log(persons);
+    });
   }
-
-
 
   render() {
     return (
@@ -39,14 +33,20 @@ class App extends Component {
           <img src={wars} alt="wars-logo" />
         </div>
         <SearchBar />
-        <Card
-          name="Greg"
-          birthday="April"
-          home="Mars"
-          photo="http://localhost:3008/darth_vader.jpg"
-        />
+
+        {this.state.persons.map(persons => {
+          return (
+            <Card
+              key={persons.id}
+              name={persons.name}
+              birthday={persons.birth_year}
+              home={persons.homeworld}
+              photo={"http://localhost:3008/" + persons.image}
+            />
+          );
+        })}
         <ul>
-          {this.state.persons.map(persons => <li> {persons.name}</li>)}
+          {/* {this.state.persons.map(persons => <li> {persons.name}</li>)} */}
         </ul>
       </div>
     );
